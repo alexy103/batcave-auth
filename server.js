f4e98f94c0dce469ce28933b57755f6022e74977
alexy103 <alexy103@hotmail.fr>
2026-06-12 14:34:14 +0200

    feat:basic-auth

--- server.js
+++ server.js
@@ -12,18 +12,20 @@ app.listen(PORT, () => {
 });
 
 app.post('/register', async (req, res) => {
-	const { username, password } = req.body;
+	const { username, password, role } = req.body;
 
 	const hash = await bcrypt.hash(password, 10);
 
 	try {
-		const insert = db.prepare('INSERT INTO users (username, password_hash) VALUES (?, ?)');
-		insert.run(username, hash);
+		const insert = db.prepare('INSERT INTO users (username, password_hash, role) VALUES (?, ?, ?)');
+		insert.run(username, hash, role);
 		res.status(201).send('Utilisateur créé avec succès !');
 	} catch (err) {
 		res.status(409).send("Erreur : l'utilisateur existe déjà.");
 	}
 });
+
+// Authenticated
 
 const checkAuth = async (req, res, next) => {
 	const authHeader = req.headers.authorization;
@@ -45,10 +47,49 @@ const checkAuth = async (req, res, next) => {
 	}
 };
 
-app.get('/bat-computer', checkAuth, (req, res) => {
-	res.sendFile(__dirname + '/private/bat-computer.html');
+app.post('/login', checkAuth, (req, res) => {
+	res.json({
+		user: {
+			id: req.user.id,
+			username: req.user.username,
+			role: req.user.role,
+		},
+	});
 });
 
-app.get('/api/secrets', checkAuth, (req, res) => {
-	res.json({ name: 'Batarang', desc: 'Arme de jet', icon: 'fa-shuriken' });
+app.get('/api/me', checkAuth, (req, res) => {
+	res.json({ id: req.user.id, username: req.user.username, role: req.user.role });
 });
+
+// Admin
+
+function adminOnly(req, res, next) {
+	if (req.user.role != 'ADMIN') return res.status(403).send('Accès refusé.');
+	next();
+}
+app.get('/bat-computer', checkAuth, adminOnly, (req, res) => {
+	res.sendFile(__dirname + '/private/bat-computer.html');
+	writeAdminLog(req);
+});
+
+app.get('/api/secrets', checkAuth, adminOnly, (req, res) => {
+	res.json([
+		{ name: 'Batarang', desc: 'Arme de jet', icon: 'fa-shuriken' },
+		{ name: 'Batmobile', desc: 'Véhicule de patrouille', icon: 'fa-car' },
+	]);
+});
+
+app.post('/api/reports', checkAuth, adminOnly, (req, res) => {
+	const { content } = req.body;
+	const insert = db.prepare('INSERT INTO reports (user_id, content) VALUES (?, ?)');
+	insert.run(req.user.id, content);
+	res.status(201).send('Rapport envoyé !');
+});
+
+// Logs
+
+function writeAdminLog(req) {
+	const timestamp = new Date().toString();
+	const insert = db.prepare('INSERT INTO logs (username, timestamp) VALUES (?, ?)');
+	insert.run(req.user.username, timestamp);
+}
